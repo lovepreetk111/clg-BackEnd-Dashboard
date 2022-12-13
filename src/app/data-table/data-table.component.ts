0247c2e4af383ebc2631cef4389f3e12543e3423
Ashish <aashish162002@gmail.com>
2022-12-13 14:13:30 +0530

    laappi pushh

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -12,78 +12,12 @@ interface Product {
 })
 export class DataTableComponent implements OnInit {
 
-  products: Product[];
-  submitted: any;
-  productDialog: any;
-  product: any;
-  dialog: any;
-  personsService: any;
-  selectedProducts: any;
-  multiItems: any;
   
 
-  constructor(private confirmationService: ConfirmationService, private messageService: MessageService) {
-    this.products = [
-     
-    ]
+  constructor() {
   }
-
   ngOnInit(): void {
-    
-  }
-  openNew() {
-    this.product = {};
-    this.submitted = false;
-    this.productDialog = true;
-  }
-  
-  deleteSelectedProducts() {
-        console.log('delete')
-    this.confirmationService.confirm({
-            message: 'Are you sure you want to delete the selected products?',
-            header: 'Confirm',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.products = this.products.filter(val => !this.selectedProducts.includes(val));
-                this.selectedProducts = null;
-                this.messageService.add({severity:'success', summary: 'Successful', detail: 'Products Deleted', life: 3000});
-            }
-        });
-    }
-
-  editProduct(product: Product) {
-    this.product = {...product};
-    this.productDialog = true;
-}
-deleteProduct(product: Product) {
-  this.confirmationService.confirm({
-      message: 'Are you sure you want to delete ' + product.name + '?',
-      header: 'Confirm',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-          this.products = this.products.filter(val => val.id !== product.id);
-          this.product = {};
-          this.messageService.add({severity:'success', summary: 'Successful', detail: 'Product Deleted', life: 3000});
       }
-  });
-}
-
-  hideDialog() {
-    this.productDialog = false;
-    this.submitted = false;
-  }
-
-  saveProduct() {
-    console.log(this.product);
-    this.products.push({
-      id: this.product.id,
-      name: ''
-    });
-    console.log(this.products)
-  }
-
-  
-
 }    
 
 
