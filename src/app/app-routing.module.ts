3c5eb2df97b284d5d57aa57f3d3aaa7955262b32
Lovepreet Kaur <klovepreet140@gmail.com>
2023-01-07 22:49:24 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataTableComponent } from './data-table/data-table.component';
+import { NoticeComponent } from './notice/notice.component';
 
 const routes: Routes = [
   {
     path:'carousel',component:DataTableComponent
+  },
+  {
+    path:'notice', component:NoticeComponent
   }
 ];
 
